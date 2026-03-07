4a77b6dc903e156f357a5ccf0d3ff48ff08203e2
leedongkyu <leedongkyu0701@gmail.com>
2026-03-07 19:48:41 +0900

    cdn-fix

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,7 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "pub-2c7d85aaca184cb4b664f61c82d16150.r2.dev",
+        hostname: "cdn.honyeojok.com",
         pathname: "/**",
       },
     ],
